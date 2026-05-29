b51cf3bb6f859ab005982e9c69e2c742d34b63e5
Sebastian Salazar <sebastian.salazar@uisek.edu.ec>
2026-05-29 16:54:38 -0500

    Configure production settings

--- examen2/settings.py
+++ examen2/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v21*!=f4q8ef_d3hj0i08chomn@^ci9ic=a#mkzu&a+=-zooy*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
